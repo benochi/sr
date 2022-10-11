d8f5ea6fa06e67eb1ed7b51cadad6e919f4e637c
Dan Politica <benochi82@gmail.com>
2022-10-11 17:29:41 -0500

    changed varibale name to be more informative.

--- sr.py
+++ sr.py
@@ -2,7 +2,7 @@ import speech_recognition as sr
 import pyttsx3
 
 #initialize the recognizer
-r = sr.Recognizer()
+mic = sr.Recognizer()
 
 #convert text to speech
 def speakText(command):
@@ -13,16 +13,18 @@ def speakText(command):
 #use mcirophone as source for input
 with sr.Microphone() as source2:
   #brief pause to adjust for surrounding volume
-  r.adjust_for_ambient_noise(source2, duration=0.1)
+  mic.adjust_for_ambient_noise(source2, duration=0.1)
   speakText("What is your name?")
   #listen for user input
-  audio2 = r.listen(source2)
+  audio2 = mic.listen(source2)
   #use google to recognize audio
   try:
-    userInput = r.recognize_google(audio2)
+    userInput = mic.recognize_google(audio2)
     userInput = userInput.lower()
     if userInput == 'dan':
-      speakText("Welcome " + userInput + "you are in control.")
+      speakText("Welcome " + userInput + "you are the boss around here.")
+    elif userInput == 'ashley':
+      speakText("Wow" + userInput + "you are looking gorgeous today.")
     else:
       speakText("your name is " + userInput)
     print(userInput)
